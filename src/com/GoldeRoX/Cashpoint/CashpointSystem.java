7d152f45eb38504502b4535e237ecb5410c1c680
Krzysztof Czupryna <k.czupryna@interia.pl>
2021-10-07 17:19:12 +0200

    Just a simplified example of a facade pattern.

--- src/com/GoldeRoX/Cashpoint/CashpointSystem.java
+++ src/com/GoldeRoX/Cashpoint/CashpointSystem.java
@@ -2,9 +2,9 @@ package com.GoldeRoX.Cashpoint;
 
 class CashpointSystem {
 
-   // void sendNotification(){
-   //     System.out.println("Text message sent to the client.");
-   // }
+    void sendNotification(){
+        System.out.println("Text message sent to the client.");
+    }
 
     boolean isMoneySecured(){
         System.out.println("ATM payment has been secured.");
